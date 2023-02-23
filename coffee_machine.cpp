f696fd7641b1ee6e86fae981fde3fee7de918a94
andreasliakos <115406856+andreasliakos@users.noreply.github.com>
2023-02-23 02:54:25 +0200

    Update coffee_machine.cpp

--- coffee_machine.cpp
+++ coffee_machine.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-int main(){
-    
-int coffee_qty = 10;
-int chocolate_qty = 10;
-int milk_qty = 20;
-double coffee_price = 1.5;
-double coffee_milk_price = 1.8;
-double chocolate_price = 2.1;
-double chocolate_milk_price = 2.4;
+
+
+
 
 void display_menu() {
     cout << "Welcome to the Coffee Machine!" << endl;
@@ -71,26 +65,62 @@ bool accept_payment(double price) {
         cout << "Insert coin or bill (10, 20, 50 cents, 1, 2, 5, 10 euros, -1 to cancel): ";
         int coin;
         cin >> coin;
-            payment += coin / 100.0;
+        payment += coin / 100.0;
         diff -= coin / 100.0;
         cout << "Amount paid: " << fixed << setprecision(2) << payment << " euros" << endl;
-        if (coin == -1){
-        return false;
+        if (coin == -1) {
+            return false;
         }
         if (coin != 10 && coin != 20 && coin != 50 && coin != 100 && coin != 200 && coin != 500 && coin != 1000) {
             cout << "Invalid coin or bill. Try again." << endl;
             continue;
         }
-    
         if (diff > 0) {
             cout << "Remaining amount: " << fixed << setprecision(2) << diff << " euros" << endl;
-        } else if (diff < 0){
+        } else if (diff < 0) {
             cout << "Change: " << fixed << setprecision(2) << -diff << " euros" << endl;
-            cout << "Thank you" 
-            }
-            
+            cout << "Thank you" << endl;
         }
-    
     }
-   
+    return true;
+}
+int main() {
+    int coffee_qty = 10;
+int chocolate_qty = 10;
+int milk_qty = 20;
+double coffee_price = 1.5;
+double coffee_milk_price = 1.8;
+double chocolate_price = 2.1;
+double chocolate_milk_price = 2.4;
+    int choice = 0;
+    bool exit_flag = false;
+    while (!exit_flag) {
+        display_menu();
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice) {
+            case 1: // Coffee
+            case 2: // Coffee with Milk
+            case 3: // Chocolate
+            case 4: // Chocolate with Milk
+                if (check_supplies(choice)) {
+                    make_drink(choice);
+                    accept_payment(get_price(choice));
+                } else {
+                    cout << "Sorry, not enough supplies to make this drink." << endl;
+                }
+                break;
+            case 5: // Refill supplies
+                refill_supplies();
+                break;
+            case -1: // Exit
+                exit_flag = true;
+                cout << "Goodbye!" << endl;
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+                break;
+        }
+    }
+    return 0;
 }
